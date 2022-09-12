94a63d74851c4accea8c297bb2464df55d604adc
javigold1 <109546149+javigold1@users.noreply.github.com>
2022-09-12 11:20:38 -0500

    more_forms

---
+++ MERN/React/more_forms/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './App.css';
+import HookForm from './components/hookForm';
+
+function App() {
+  return (
+    <div className="App">
+      <HookForm />
+    </div>
+  );
+}
+
+export default App;
