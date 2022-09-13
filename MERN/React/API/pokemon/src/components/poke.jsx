ec3b9f94dd37deecf2d2eb31367eeddfd5ae9798
javigold1 <109546149+javigold1@users.noreply.github.com>
2022-09-13 10:33:28 -0500

    third

--- MERN/React/API/pokemon/src/components/poke.jsx
+++ MERN/React/API/pokemon/src/components/poke.jsx
@@ -1,30 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-const PokeComponent = (props) => {
-    const [responseData, setResponseData] = useState([]);
-    
-    useEffect (() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
-        .then((response) => {
-            console.log(response.data.results);
-            setResponseData(response.data.results);
-            console.log(responseData)
-        }
-        )
-        .catch((err) => console(err));
-    }, [] )
+const PokemonList = () => {
 
-    return(
+    const [pokemonList, setPokemonList] = useState([]);
+
+    const handleOnClick = async () => {
+        try {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=807');
+            console.log(response.data.results);
+            setPokemonList((listaPrev) => response.data.results);
+        } catch (err) {
+                console.error(err);
+        }
+    }
+
+    return (
         <div>
-            <button>Fetch Pokemon</button>
-            {responseData.map((pokemon, index) => {
-                return(
-                <div key={index}>
-                    <p>{pokemon.name}</p>
-                </div>
-            )})}
+            <button className='btn btn-primary' onClick={handleOnClick}>Fetch Pokemon</button>
+            <ul className='col-4 mx-auto'>
+                {
+                    pokemonList ? pokemonList.map((item, index) => {
+                        return (
+                            <li key={index}>{item.name}</li>
+                        );
+                    })
+                        : ''
+                }
+            </ul>
         </div>
     )
+
 }
-export default PokeComponent;
+export default PokemonList;
