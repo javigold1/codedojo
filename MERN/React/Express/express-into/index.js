162eebf98c696dc17f925a5371d54a5543709bbb
javigold1 <109546149+javigold1@users.noreply.github.com>
2022-09-19 09:28:42 -0500

    first

--- MERN/React/Express/express-into/index.js
+++ MERN/React/Express/express-into/index.js
@@ -1,15 +1,37 @@
 const port = 8000;
 const { application } = require('express');
 const express = require('express');
+const faker = require('@faker-js/faker');
 const app = express();
+
 
 app.use( express.json() );
 
+// var city = faker.address.city();
+
+// console.log(
+//     faker.helpers.fake(
+//       'Hello {{name.prefix}} {{name.lastName}}, how are you today?'
+//     )
+//   );
+
 const indexCallBack = (req,res) => {
-    return res.json({hello : 'world', someOtherKey: "nother"})
+    return res.json({hello : 'world', someOtherKey: city})
 }
 
-app.get("/", indexCallBack)
+// app.get("/", indexCallBack)
+
+class User {
+    constructor() {
+    //   this._id = faker.finance.account();
+      this.city = faker.address.city()
+      this.firstName = faker.name.firstName();
+      this.lastName = faker.name.lastName();
+      this.phoneNumber = faker.phone.phoneNumber();
+      this.email = faker.internet.email();
+      this.password = faker.internet.password();
+    }
+  }
 
 app.get("/api/cities", (req,res) => {
     const cities = [
@@ -33,6 +55,13 @@ app.get("/api/cities", (req,res) => {
     return res.json(cities);
 
 });
+
+app.get("/api/users/new", (req, res) => {
+    const user = new User();
+    console.log("Navigated to /api/users/new ||", JSON.stringify(uer));
+    res.send(user);
+  });
+  
 
 app.post('/api/cities', (req,res) => {
     console.log(req.body)
