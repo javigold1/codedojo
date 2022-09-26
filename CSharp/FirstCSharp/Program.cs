bf49765d930ccc0a64166463bfd7dc9db7176cbe
javigold1 <109546149+javigold1@users.noreply.github.com>
2022-09-26 16:47:46 -0500

    Fundamentals I

--- CSharp/FirstCSharp/Program.cs
+++ CSharp/FirstCSharp/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void WriteInts()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        bool isDivisibleBy3 = i % 3 == 0;
+        bool isDivisibleBy5 = i % 5 == 0;
+        bool isDivisibleBy3and5 = isDivisibleBy3 && isDivisibleBy5;
+        bool isDivisibleBy3or5 = isDivisibleBy3 || isDivisibleBy5;
+
+        if (isDivisibleBy3and5)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (isDivisibleBy3)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (isDivisibleBy5)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
+
+WriteInts();
+
